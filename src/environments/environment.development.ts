79cdf3121607aba5eed9ef4d34f932fb73409eae
HedSut <79856044+HedSut@users.noreply.github.com>
2024-05-12 19:53:15 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,11 +7,5 @@ export const environment = {
     configCollection: "Configs",
     userCollection: "Users",
     firebase: {
-        "projectId":"pc-part-picker-dd7a9",
-        "appId":"1:229358292099:web:2d8500d85d17b410c15fb6",
-        "storageBucket":"pc-part-picker-dd7a9.appspot.com",
-        "apiKey":"AIzaSyBwgVm4y908nFiXdQpP0AjMKCZEZok42KA",
-        "authDomain":"pc-part-picker-dd7a9.firebaseapp.com",
-        "messagingSenderId":"229358292099",
-        "measurementId":"G-BG2KLCYK8Q"}
+        "hellnaw": "hellnaw"}
 };
